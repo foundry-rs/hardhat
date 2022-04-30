2374c4e9132c781fe2ed8b189027206f6ff1af83
Matthias Seitz <matthias.seitz@outlook.de>
2022-04-30 14:41:58 +0200

    feat: add waffle adapter

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
 import debug from "debug";
 import { TASK_RUN, TASK_TEST } from "hardhat/builtin-tasks/task-names";
-import { extendConfig, task } from "hardhat/config";
+import { extendConfig, task, extendEnvironment } from "hardhat/config";
 import {
   HardhatRuntimeEnvironment,
   RunSuperFunction,
   TaskArguments,
 } from "hardhat/types";
+import { lazyObject } from "hardhat/plugins";
 
 const log = debug("hardhat:plugin:anvil");
 
 import { AnvilService } from "./anvil-service";
+import { AnvilProviderAdapter } from "./anvil-provider-adapter";
+
+extendEnvironment((hre) => {
+  // This mimics the `hardhat-waffle` plugin so that it works as replacement for 
+  // it this is currently necessary because the waffle plugin is bound to a network
+  // with the name `hardhat`
+  (hre as any).waffle = lazyObject(() => {
+    const hardhatAnvilProvider = new AnvilProviderAdapter(
+      hre.network
+    ) as any;
+
+    return {
+      provider: hardhatAnvilProvider,
+    };
+  });
+});
 
 task(TASK_TEST, async (_args, env, runSuper) => {
   return handlePluginTask(env, runSuper);
