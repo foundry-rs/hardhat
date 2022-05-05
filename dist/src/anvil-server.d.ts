19cea5c28e0e14eca8f2bfbd005f734f7dcf4adf
Matthias Seitz <matthias.seitz@outlook.de>
2022-05-05 17:13:15 +0200

    chore: cleanup

--- dist/src/anvil-server.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class AnvilServer {
-    private readonly _anvil;
-    private readonly _options;
-    private constructor();
-    static launch(options: any): AnvilServer;
-    kill(): void;
-}
-//# sourceMappingURL=anvil-server.d.ts.map
--- dist/src/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const pluginName = "@foundry/hardhat-anil";
-//# sourceMappingURL=constants.d.ts.map
