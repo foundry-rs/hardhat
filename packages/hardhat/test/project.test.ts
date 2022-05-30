09ae44e44ec59f2a6b499f090db85491bdcde76a
Matthias Seitz <matthias.seitz@outlook.de>
2022-05-30 03:10:39 +0200

    test: add test empty config is sufficient (#30)
    * test: add test setting defaultNetwork is sufficient
    
    * test: update tests

--- packages/hardhat/test/project.test.ts
+++ packages/hardhat/test/project.test.ts
@@ -1,6 +1,8 @@
 // tslint:disable-next-line no-implicit-dependencies
 // eslint-disable-next-line import/no-extraneous-dependencies
 
+import { assert } from "chai";
+import { AnvilService } from "@foundry-rs/hardhat-anvil/src/anvil-service";
 import { useEnvironment } from "./helpers";
 
 describe("Integration tests", function () {
@@ -13,6 +15,26 @@ describe("Integration tests", function () {
     });
 
     it("Should add run anvil node", async function () {
+      void this.hre.run("node");
+      // ensure we don't wait forever
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    });
+  });
+  describe("Hardhat Runtime Environment extension", function () {
+    useEnvironment("no-anvil-config");
+
+    it("Should expose anvil defaults configs in hardhat's config", function () {
+      assert.isDefined(this.hre.config.networks.anvil);
+      const defaultOptions = AnvilService.getDefaultOptions() as any;
+      const options = this.hre.config.networks.anvil as any;
+
+      // Iterate over all default options and assert equality
+      for (const [key, value] of Object.entries(defaultOptions)) {
+        assert.equal(options[key], value);
+      }
+    });
+
+    it.only("Should add run anvil node", async function () {
       void this.hre.run("node");
       // ensure we don't wait forever
       await new Promise((resolve) => setTimeout(resolve, 5000));
