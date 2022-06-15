e02aee0ea8d83594700a94b1a120805f8e1980d8
Mark Tyneway <mark.tyneway@gmail.com>
2022-06-15 08:47:36 -0700

    Merge develop into master (#55)
    * fix(hh/forge): typechain support (#45)
    
    Typechain uses a private method on the artifact
    with an unsafe `any` typecast to get artifact paths.
    To work with typechain, this method must exist until
    typechain is updated.
    
    The issue is here: https://github.com/dethcrypto/TypeChain/issues/544
    
    * feat: `Artifacts.getBuildInfo` (#48)
    
    Implement a naive `getBuildInfo` function. This is useful
    for tooling that integrates more deeply with the hardhat ecosystem.
    Note that this implementation is not 100% with the output
    that the hardhat compiler toolchain outputs, but the output
    is good enough to get more tooling to be compatible.
    
    * hardhat-forge: use the correct source name (#51)
    
    The relative path to the file from the root
    of the project should be used as the source name
    in the artifact.
    
    Closes https://github.com/foundry-rs/hardhat/issues/36
    
    This will fix tooling that depends on the source name:
    https://github.com/defi-wonderland/smock/blob/27fe7a5f3c351714257cdc98204e282932c11e12/src/utils/storage.ts#L54
    
    * test: explicitly set port for anvil (#53)
    
    * test: explicitly set port for anvil
    
    * chore: add changeset
    
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- packages/hardhat/test/helpers.ts
+++ packages/hardhat/test/helpers.ts
@@ -1,6 +1,7 @@
 import { resetHardhatContext } from "hardhat/plugins-testing";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import path from "path";
+import net from "net";
 
 declare module "mocha" {
   interface Context {
@@ -9,13 +10,24 @@ declare module "mocha" {
 }
 
 export function useEnvironment(fixtureProjectName: string) {
-  beforeEach("Loading hardhat environment", function () {
+  beforeEach("Loading hardhat environment", async function () {
     process.chdir(path.join(__dirname, "fixture-projects", fixtureProjectName));
 
     this.hre = require("hardhat");
+    this.freePort = await getPortFree();
   });
 
   afterEach("Resetting hardhat", function () {
     resetHardhatContext();
+  });
+}
+
+async function getPortFree() {
+  return new Promise((res) => {
+    const srv = net.createServer() as any;
+    srv.listen(0, () => {
+      const port = srv.address().port;
+      srv.close((_err: any) => res(port));
+    });
   });
 }
