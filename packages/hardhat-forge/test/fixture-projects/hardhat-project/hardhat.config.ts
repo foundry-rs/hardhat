05eb3da7fa3cb4039ac315b383ee0593f69600c8
Mark Tyneway <mark.tyneway@gmail.com>
2022-06-23 14:49:24 -0700

    feat(hardhat-forge): add `foundry` config section
    The `hardhat.config.ts` now can have a `foundry` section that
    allows users to place config values in. This makes it so that
    expensive operations like generating the build info can be placed
    in the `hardhat.config.ts` and not in the `foundry.toml` so that
    `forge build` can be ligher than `hardhat compile`.

--- packages/hardhat-forge/test/fixture-projects/hardhat-project/hardhat.config.ts
+++ packages/hardhat-forge/test/fixture-projects/hardhat-project/hardhat.config.ts
@@ -5,7 +5,8 @@ import "../../../src/index";
 
 const config: HardhatUserConfig = {
   solidity: "0.7.3",
-  defaultNetwork: "hardhat"
+  defaultNetwork: "hardhat",
+  foundry: {},
 };
 
 export default config;
